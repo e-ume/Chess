1b07db3039e1151d5b20f7af59d3009c6c2c484e
e-ume <umeemeka@gmail.com>
2020-01-13 13:00:12 -0500

    2nd Commit

--- src/Capabilities/Diagonal/DiagonalUpRight.scala
+++ src/Capabilities/Diagonal/DiagonalUpRight.scala
@@ -1,14 +1,14 @@
-package Capabilities.Digonal
+package Capabilities.Diagonal
 
-trait DiagonalDownLeft {
-
+trait DiagonalUpRight {
+  //Max Blast Radius will be provided by the chess object
   def computeBlastRadius(p: (Int, Int), MaxBlastRadius: (Int, Int)): Set[(Int, Int)] = {
     var s: Set[(Int, Int)] = Set()
     var tracker: (Int, Int) = (p._1, p._2) //Init with current point of object & tracks location
 
-    while(tracker._1 >= MaxBlastRadius._1 && tracker._2 >= MaxBlastRadius._2) {
+    while(tracker._1 <= MaxBlastRadius._1 && tracker._2 <= MaxBlastRadius._2) {
       s = s ++ Set(tracker)
-      tracker = (tracker._1 - 1,tracker._2 - 1)
+      tracker = (tracker._1 + 1,tracker._2 + 1)
     }
     s
   }
