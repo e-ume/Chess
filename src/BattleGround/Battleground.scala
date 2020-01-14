4d05ae279c12435e38c78a2e8b0a3c4808a87207
e-ume <umeemeka@gmail.com>
2020-01-13 19:45:40 -0500

    6th Commit

---
+++ src/BattleGround/Battleground.scala
@@ -0,0 +1,69 @@
+package BattleGround
+
+import Objects.MaxBlastRadius
+import Westeros.{Bishop, King, Knight, Queen}
+
+import scala.util.Random
+
+class Battleground(var m: Map[String,Int], var M: Int, var N: Int) {
+  var kings = new Array[King](m.get("kings").get) //2 Kings
+  var queens = new Array[Queen](m.get("queens").get) // 1 queen
+  var bishops = new Array[Bishop](m.get("bishop").get)
+  var knights = new Array[Knight](m.get("knight").get)
+
+  def getPosition(): (Int, Int) = {
+    (scala.util.Random.between(1, M + 1), scala.util.Random.between(1, N + 1))
+  }
+
+  def prepareForBattle(): Unit ={
+    if(kings.length != 0) {
+      for (n <- 1 to kings.length) {
+        val position = getPosition()
+        kings(n) = new King(position._1,position._2)
+      }
+    }
+
+    if(queens.length != 0) {
+      for (n <- 1 to queens.length) {
+        val position = getPosition()
+        queens(n) = new Queen(position._1,position._2)
+      }
+    }
+
+    if(bishops.length != 0) {
+      for (n <- 1 to bishops.length) {
+        val position = getPosition()
+        bishops(n) = new Bishop(position._1,position._2)
+      }
+    }
+
+    if(knights.length != 0) {
+      for (n <- 1 to knights.length) {
+        val position = getPosition()
+        knights(n) = new Knight(position._1,position._2)
+      }
+    }
+
+  }
+
+  def outcome(): {}
+  def checkCasualties(): {}
+}
+
+object Battleground {
+  val battles = 10
+  var battle = 0
+  def main(args: Array[String]): Unit = {
+    var map :Map[String,Int] = Map()
+    var M = 7
+    var N = 7
+
+    while (battle < battles) {
+      val dimension = MaxBlastRadius(M,N)
+      var battleground = new Battleground(map, M, N)
+      println(battle)
+      battle = battle + 1
+    }
+
+  }
+}
--- src/Objects/MaxBlastRadius.scala
+++ src/Objects/MaxBlastRadius.scala
@@ -1,8 +1,12 @@
 package Objects
 
 object MaxBlastRadius extends Enumeration {
+
   val LowerLeft = (1,1)
   val LowerRight = (7,1)
   val UpperLeft = (1,7)
   val UpperRight = (7,7)
 }
+
+
+
